75c06e3133c870c543d151af7a1460858b1bf898
Hieu Mau <mauhieu12@gmail.com>
2022-12-15 01:26:03 +0700

    Add register api work

--- src/app/shared/page/authen/register/register.component.ts
+++ src/app/shared/page/authen/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+import {AuthService} from "../../../../core/service/auth.service";
+import {User} from "../../../model/user.model";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  user: User = new User()
+  rePass: string = ''
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService,
+              private router: Router,
+              private snackBar: MatSnackBar) {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  register() {
+    if (!this.validate()) return
+    this.authService.register(this.user).then(result => {
+      this.snackBar.open('Register successfully.', '', {duration: 3000})
+      this.user = new User()
+      this.router.navigate(['login'])
+    })
+  }
+
+  private validate(): boolean {
+    let result = true
+    if (this.rePass != this.user.password) {
+      result = false
+      this.snackBar.open('Repeat password not correct!', '', {duration: 5000})
+    }
+    return result
+  }
 }
